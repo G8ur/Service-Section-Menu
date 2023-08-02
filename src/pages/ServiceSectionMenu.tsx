f4a4532591963a858e02abe3e2b669f1126a3ba5
G8ur <G8ur>
2023-08-02 14:09:52 +0000

    First Commit from Locofy Builder

---
+++ src/pages/ServiceSectionMenu.tsx
@@ -0,0 +1,346 @@
+import { FunctionComponent } from "react";
+import styles from "./ServiceSectionMenu.module.css";
+const ServiceSectionMenu: FunctionComponent = () => {
+  return (
+    <div className={styles.serviceSectionMenu}>
+      <img
+        className={styles.serviceSectionMenuChild}
+        alt=""
+        src="/rectangle-79@2x.png"
+      />
+      <div className={styles.carpentry}>Carpentry</div>
+      <div className={styles.serviceSectionMenuItem} />
+      <div className={styles.serviceSectionMenuInner} />
+      <div className={styles.quickFix}>
+        <span className={styles.quickFixTxtContainer}>
+          <span>QUICK</span>
+          <span className={styles.fix}> FIX</span>
+        </span>
+      </div>
+      <div className={styles.register}>Register</div>
+      <div className={styles.loginSign}>Login / Sign Up</div>
+      <div className={styles.divplaceholder} />
+      <div className={styles.div}>|</div>
+      <div className={styles.lineDiv} />
+      <div className={styles.header}>
+        <div className={styles.aboutUs}>{`About Us `}</div>
+        <div className={styles.contactUs}>
+          <span className={styles.contact}>Contact</span>
+          <span>{` `}</span>
+          <span className={styles.contact}>Us</span>
+          <span>{` `}</span>
+        </div>
+        <div className={styles.home}>Home</div>
+        <div className={styles.servicesParent}>
+          <div className={styles.services}>Services</div>
+          <div className={styles.div1}></div>
+        </div>
+      </div>
+      <div className={styles.text} />
+      <div className={styles.home1}>Home</div>
+      <div className={styles.found12Results}>Found 12 Results</div>
+      <div className={styles.carpentry1}>Carpentry</div>
+      <div className={styles.component3}>
+        <div className={styles.component3Child} />
+        <img className={styles.icon} alt="" src="/menu-2-1@2x.png" />
+      </div>
+      <div className={styles.component4}>
+        <div className={styles.component3Child} />
+        <img className={styles.icon} alt="" src="/6888292-1@2x.png" />
+      </div>
+      <div className={styles.rectangleParent}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 4000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-1@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div2}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.serviceType}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName}>
+          <div className={styles.commercialCarpentryServices}>
+            Architectural carpentry
+          </div>
+        </div>
+        <div className={styles.bookButton}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.rectangleGroup}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 5000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-11@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div4}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.bookButton1}>
+          <div className={styles.bookButtonItem} />
+          <div className={styles.bookNow1}>Book Now</div>
+        </div>
+        <div className={styles.serviceType1}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName1}>
+          <div className={styles.commercialCarpentryServices}>
+            Restoration carpentry
+          </div>
+        </div>
+        <div className={styles.bookButton2}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.rectangleContainer}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 6000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-12@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div2}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.serviceType}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName}>
+          <div className={styles.commercialCarpentryServices}>
+            Modeling carpentry
+          </div>
+        </div>
+        <div className={styles.bookButton}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.frameDiv}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 2000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-13@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div4}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.bookButton1}>
+          <div className={styles.bookButtonItem} />
+          <div className={styles.bookNow1}>Book Now</div>
+        </div>
+        <div className={styles.serviceType1}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName3}>
+          <div className={styles.commercialCarpentryServices}>
+            Cabinetmaking
+          </div>
+        </div>
+        <div className={styles.bookButton2}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.rectangleParent1}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 3000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-14@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div2}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.serviceType}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName}>
+          <div className={styles.commercialCarpentryServices}>
+            Wooden Carpentry Work
+          </div>
+        </div>
+        <div className={styles.bookButton}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.rectangleParent2}>
+        <div className={styles.frameChild} />
+        <div className={styles.inr4000}>INR 7000/-</div>
+        <div className={styles.indore}>Indore</div>
+        <div className={styles.services10300x2001Wrapper}>
+          <div className={styles.services10300x2001}>
+            <img
+              className={styles.services10300x2001Icon}
+              alt=""
+              src="/services10300x200-15@2x.png"
+            />
+          </div>
+        </div>
+        <div className={styles.frameItem} />
+        <div className={styles.div2}>4</div>
+        <div className={styles.ifaSolid}>
+          <div className={styles.div3}></div>
+        </div>
+        <div className={styles.serviceType}>
+          <div className={styles.serviceTypeChild} />
+          <div className={styles.carpentry2}>Carpentry</div>
+        </div>
+        <div className={styles.serviceTypeName}>
+          <div className={styles.commercialCarpentryServices}>
+            Formwork carpentry
+          </div>
+        </div>
+        <div className={styles.bookButton}>
+          <div className={styles.bookButtonChild} />
+          <div className={styles.bookNow}>Book Now</div>
+        </div>
+      </div>
+      <div className={styles.component5}>
+        <div className={styles.component3Child} />
+        <div className={styles.div14}>1</div>
+      </div>
+      <div className={styles.component6}>
+        <div className={styles.component3Child} />
+        <div className={styles.div15}></div>
+      </div>
+      <div className={styles.component51}>
+        <div className={styles.component3Child} />
+        <div className={styles.div14}>2</div>
+      </div>
+      <div className={styles.serviceSectionMenuChild1} />
+      <div className={styles.linkTerms}>Terms and Conditions</div>
+      <div className={styles.itemLink}>Privacy</div>
+      <div className={styles.input} />
+      <div className={styles.enterYourEmail}>Enter your email</div>
+      <div className={styles.heading2}>Newsletter Signup</div>
+      <div className={styles.loremIpsumDolorContainer}>
+        <p className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit consectetur
+        </p>
+        <p className={styles.loremIpsumDolor}>
+          adipiscing elit, sed do eiusmod tempor
+        </p>
+        <p className={styles.loremIpsumDolor}>commodo consequat</p>
+      </div>
+      <div className={styles.quickFix1}>
+        <span className={styles.quickFixTxtContainer}>
+          <span>QUICK</span>
+          <span className={styles.fix}> FIX</span>
+        </span>
+      </div>
+      <div className={styles.loremIpsumDolorContainer1}>
+        <p className={styles.loremIpsumDolor}>
+          Lorem ipsum dolor sit consectetur
+        </p>
+        <p className={styles.loremIpsumDolor}>
+          adipiscing elit, sed do eiusmod tempor
+        </p>
+        <p className={styles.loremIpsumDolor}>commodo consequat</p>
+      </div>
+      <div className={styles.link}>
+        <div className={styles.div17}></div>
+      </div>
+      <div className={styles.link1}>
+        <div className={styles.div17}></div>
+      </div>
+      <div className={styles.link2}>
+        <div className={styles.div17}></div>
+      </div>
+      <div className={styles.link3}>
+        <div className={styles.div17}></div>
+      </div>
+      <div className={styles.heading21}>Follow Us</div>
+      <div className={styles.wwwexamplecom}>www.example.com</div>
+      <div className={styles.businessMailId}>Business Mail Id</div>
+      <div className={styles.contactNo}>Contact No.</div>
+      <div
+        className={styles.shrinathEnclaveRoyal}
+      >{`Shrinath Enclave Royal Ciity `}</div>
+      <div className={styles.heading22}>Contact Us</div>
+      <div className={styles.linkServices}>About Us</div>
+      <div className={styles.linkCategories}>Services</div>
+      <div className={styles.linkAbout}>Home</div>
+      <div className={styles.heading23}>Quick Links</div>
+      <div className={styles.listboxComponent}>
+        <div className={styles.listboxMain}>
+          <div className={styles.listboxbg} />
+          <img className={styles.chevronIcon} alt="" src="/chevron.svg" />
+          <div className={styles.placeholderText}>
+            <div className={styles.filter}>Filter</div>
+          </div>
+        </div>
+        <div className={styles.clipList}>
+          <div className={styles.dropdownList}>
+            <div className={styles.item1}>
+              <div className={styles.div21}>Highest Rated</div>
+            </div>
+            <div className={styles.item1}>
+              <div className={styles.div21}>{`Most Reviewed `}</div>
+            </div>
+            <div className={styles.item1}>
+              <div className={styles.div21}>Featured</div>
+            </div>
+            <div className={styles.item1}>
+              <div className={styles.div21}>Newest</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ServiceSectionMenu;
